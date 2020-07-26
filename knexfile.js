cd17175c01a649860acd49282f67238bf2b4dcae
Pere <pere.solaclaver@gmail.com>
2020-07-26 23:52:08 +0100

    Change env variable for pg database in heroku

--- knexfile.js
+++ knexfile.js
@@ -16,7 +16,7 @@ module.exports = {
 
     production: {
         client: 'pg',
-        connection: process.env.DATABASE_PROD_URL,
+        connection: process.env.DATABASE_URL,
         migrations: {
             directory: './db/migrations/prod',
         },
