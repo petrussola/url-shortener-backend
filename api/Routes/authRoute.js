1d9cee97a8e6cbba157a251fc53d607bb1582d01
Pere <pere.solaclaver@gmail.com>
2020-07-29 23:35:32 +0100

    Create model to register new User

--- api/Routes/authRoute.js
+++ api/Routes/authRoute.js
@@ -1,13 +1,25 @@
 const express = require('express');
 
+// models
+const { createUser } = require('../Models/UserModel');
+
 // middleware
 const { hashPassword } = require('../Middleware/UserAuth');
 
 const router = express.Router();
 
-router.post('/signup', [hashPassword], (req, res) => {
+router.post('/signup', [hashPassword], async (req, res) => {
     const { email, password } = req.body;
-    res.status(200).json({ message: 'ok' });
+    try {
+        const newUser = await createUser({ email, password });
+        res.status(200).json({
+            status: 'success',
+            message: 'user created',
+            data: newUser,
+        });
+    } catch (error) {
+        res.status(500).json({ status: 'fail', message: error.message });
+    }
 });
 
 module.exports = router;
