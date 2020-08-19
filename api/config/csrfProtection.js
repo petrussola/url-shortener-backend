9af678d9f39626c50f5b0b7126d8faff56d5d47b
Pere <pere.solaclaver@gmail.com>
2020-08-20 00:28:56 +0100

    Remove same site config in production

--- api/config/csrfProtection.js
+++ api/config/csrfProtection.js
@@ -17,7 +17,7 @@ if (process.env.NODE_ENV === 'development') {
             httpOnly: process.env.HTTP_ONLY,
             secure: process.env.SECURE,
             domain: process.env.CSRF_PROTECTION_HOST, // host (NOT DOMAIN, NOT HTTP:// OR HTTPS://)!
-            sameSite: process.env.SAME_SITE,
+            // sameSite: process.env.SAME_SITE,
         },
     });
 }
