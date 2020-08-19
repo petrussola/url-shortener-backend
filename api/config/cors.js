ea03157d96e1637e548d199a412d2d53d2f3ab14
Pere <pere.solaclaver@gmail.com>
2020-08-19 23:37:52 +0100

    Add frontend origin to allow cors from subdomain

--- api/config/cors.js
+++ api/config/cors.js
@@ -10,7 +10,7 @@ if (process.env.NODE_ENV === 'development') {
     };
 } else {
     corsConfig = {
-        origin: false,
+        origin: process.env.DEV_FRONTEND_URL,
         optionsSuccessStatus: 200,
     };
 }
