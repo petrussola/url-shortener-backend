6f863dcff1ba620fb715d760342234c5e71a49b9
Pere <pere.solaclaver@gmail.com>
2020-08-20 00:45:38 +0100

    add domain

--- api/config/csrfProtection.js
+++ api/config/csrfProtection.js
@@ -16,7 +16,7 @@ if (process.env.NODE_ENV === 'development') {
             // maxAge: 60 * 60 * 24, // 1 day in seconds
             httpOnly: process.env.HTTP_ONLY,
             secure: process.env.SECURE,
-            // domain: process.env.CSRF_PROTECTION_HOST, // host (NOT DOMAIN, NOT HTTP:// OR HTTPS://)!
+            domain: process.env.CSRF_PROTECTION_HOST, // host (NOT DOMAIN, NOT HTTP:// OR HTTPS://)!
             // sameSite: process.env.SAME_SITE,
         },
     });
