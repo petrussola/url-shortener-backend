8a248b7254f9093ad58d4ab5b9c6a52fba05bcad
Pere <pere.solaclaver@gmail.com>
2020-08-20 23:19:53 +0100

    Add credentials

--- api/config/cors.js
+++ api/config/cors.js
@@ -12,6 +12,7 @@ if (process.env.NODE_ENV === 'development') {
     corsConfig = {
         origin: process.env.DEV_FRONTEND_URL,
         optionsSuccessStatus: 200,
+        credentials: true, // sets Access-Control-Allow-Credentials Header
     };
 }
 
